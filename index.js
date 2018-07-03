34cecee07be191584a81de8e4bf2bdd9f44c069a
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-03 18:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function getRepositories() {
 function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
+  req.addEventListener("load", showCommits);
   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
   req.send()
 }
