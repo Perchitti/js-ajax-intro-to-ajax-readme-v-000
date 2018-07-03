674bec7d5d8076ed7a1b9cb8b687db1b69b4a8f5
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-03 18:24:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+function showRepositories(event, data) {
+  console.log(this.responseText)
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET", 'https://api.github.com/users/octocat/repos')
